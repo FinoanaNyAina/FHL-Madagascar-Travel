b82691bc4b1ede7a3a7d2e84a0adc9357e0cf5a4
FinoanaNyAina <finoanaandriantsoa@gmail.com>
2023-06-04 15:47:56 +0300

    first boutton

--- src/page/components/home/Home.jsx
+++ src/page/components/home/Home.jsx
@@ -4,7 +4,25 @@ import "../home/Home.css";
 function Home() {
     return (
         <>
-            <p>pourquoi</p>
+            <div className="body">
+                <header>
+                    <div >
+                        <button className="btnLeft btn">LOGO</button>
+                    </div>
+                    <div >
+                        <div className="divBtnCenter">
+                            <button className="btnCenter btn">Work</button>
+                            <button className="btnCenter btn">Distraction</button>
+                            <button className="btnCenter btn">Contact</button>
+                        </div>
+                        
+                    </div>
+                    <div >
+                        <button className="btnRight btn">MENU</button>
+                    </div>
+                </header>
+            </div>
+
         </>
     )
 }
